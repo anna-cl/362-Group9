8c9da84f6ff390277e5d5e28a89aa7cb9cf7cb53
Anna Chaoer Liang <anna_liang_2@sfu.ca>
2022-11-09 12:11:13 -0800

    implement animation

--- WeCareApp/app/src/main/java/ca/cmpt362/projects/WeCareApp/SplashScreenActivity.kt
+++ WeCareApp/app/src/main/java/ca/cmpt362/projects/WeCareApp/SplashScreenActivity.kt
@@ -2,14 +2,33 @@ package ca.cmpt362.projects.weCareApp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.animation.Animation
+import android.view.animation.AnimationUtils
+import android.widget.ImageView
+import android.widget.TextView
 
 
 //Citation:
 //https://www.youtube.com/watch?v=Iajl3LEmqKo&ab_channel=CodingwithDev
+//https://www.youtube.com/watch?v=JLIFqqnSNmg&ab_channel=CodingWithTea
+//https://www.geeksforgeeks.org/how-to-create-a-splash-screen-in-android-using-kotlin/
 
 class SplashScreenActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_splash_screen)
+
+        val topAnimation: Animation = AnimationUtils.loadAnimation(this, R.anim.top_animation)
+        val bottomAnimation = AnimationUtils.loadAnimation(this, R.anim.bottom_animation)
+
+        val splashLogo: ImageView = findViewById(R.id.splash_logo)
+        val splashAppName:TextView = findViewById(R.id.splash_app_name)
+        val splashSlogan: TextView = findViewById(R.id.splash_slogan)
+
+        splashLogo.animation = topAnimation
+        splashAppName.animation = bottomAnimation
+        splashSlogan.animation = bottomAnimation
+
     }
 }
