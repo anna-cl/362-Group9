0c4739a81f0d3a444f5e6caa61e22abd905a301e
Anna Chaoer Liang <anna_liang_2@sfu.ca>
2022-11-09 11:36:40 -0800

    styling splash screen xml

---
+++ WeCareApp/app/src/main/java/ca/cmpt362/projects/WeCareApp/SplashScreenActivity.kt
@@ -0,0 +1,15 @@
+package ca.cmpt362.projects.weCareApp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+
+//Citation:
+//https://www.youtube.com/watch?v=Iajl3LEmqKo&ab_channel=CodingwithDev
+
+class SplashScreenActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash_screen)
+    }
+}
