b47f87db6f0ffbf557ac2e0e4113b18f6bc7a0f2
harpreetdubb <dubbharpreet@gmail.com>
2022-11-14 21:38:05 -0800

    login, signup, forgot password

--- WeCareApp/app/src/main/java/ca/cmpt362/projects/WeCareApp/SignupActivity.kt
+++ WeCareApp/app/src/main/java/ca/cmpt362/projects/WeCareApp/SignupActivity.kt
@@ -7,18 +7,20 @@ import android.os.Bundle
 import android.view.View
 import androidx.appcompat.widget.Toolbar
 
-class ForgotPasswordActivity : AppCompatActivity() {
+class SignupActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_forgot_password)
+        setContentView(R.layout.activity_signup)
         val toolbar = findViewById<Toolbar>(R.id.toolbar)
         setSupportActionBar(toolbar)
         supportActionBar?.setDisplayShowTitleEnabled(false)
         toolbar.setNavigationOnClickListener { super.onBackPressed() }
-        setStatusBarWhite(this@ForgotPasswordActivity)
+        setStatusBarWhite(this@SignupActivity)
     }
-    fun setStatusBarWhite(activity: AppCompatActivity){
+
+    private fun setStatusBarWhite(activity: AppCompatActivity){
+        //Make status bar icons color dark
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
             activity.window.decorView.systemUiVisibility = View.SYSTEM_UI_FLAG_LIGHT_STATUS_BAR
             activity.window.statusBarColor = Color.WHITE
