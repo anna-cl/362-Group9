6b93ff04197c263ee25c11b1447c30866f479eab
Shraousy <sss56@sfu.ca>
2022-11-26 23:07:46 -0800

    App connected to firebase

--- WeCareApp/app/src/main/java/ca/cmpt362/projects/WeCareApp/ForgotPasswordActivity.kt
+++ WeCareApp/app/src/main/java/ca/cmpt362/projects/WeCareApp/ForgotPasswordActivity.kt
@@ -5,10 +5,16 @@ import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
+import android.widget.Button
+import android.widget.EditText
+import android.widget.Toast
 import androidx.appcompat.widget.Toolbar
+import com.google.firebase.auth.FirebaseAuth
 
 class ForgotPasswordActivity : AppCompatActivity() {
-
+    private lateinit var resetWithEmail:EditText
+    private lateinit var forgotPassTV: Button
+    private lateinit var firebaseAuth: FirebaseAuth
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_forgot_password)
@@ -17,6 +23,22 @@ class ForgotPasswordActivity : AppCompatActivity() {
         supportActionBar?.setDisplayShowTitleEnabled(false)
         toolbar.setNavigationOnClickListener { super.onBackPressed() }
         setStatusBarWhite(this@ForgotPasswordActivity)
+
+        firebaseAuth = FirebaseAuth.getInstance()
+        resetWithEmail = findViewById(R.id.et_email)
+        forgotPassTV = findViewById(R.id.button_send)
+        forgotPassTV.setOnClickListener{
+            var getResetMail = resetWithEmail.text.toString()
+            firebaseAuth.sendPasswordResetEmail(getResetMail).addOnSuccessListener {
+                // notify listener if reset link has been sent to their mailbox
+                Toast.makeText(this, "Reset link has been sent to your email", Toast.LENGTH_SHORT).show()
+            }.addOnFailureListener{
+                // notify listener if system has detected some error and display exception message
+                // possible errors: email entered is invalid, email entered was not account email (not used for signup in the app)
+                exception: java.lang.Exception -> Toast.makeText(this, exception.message, Toast.LENGTH_SHORT).show()
+            }
+
+        }
     }
     fun setStatusBarWhite(activity: AppCompatActivity){
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
