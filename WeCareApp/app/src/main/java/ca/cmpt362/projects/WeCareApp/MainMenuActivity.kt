c0f1466848a4c4719356ed9a4a5ae7650c6e5961
Shraousy <sss56@sfu.ca>
2022-11-29 12:28:21 -0800

    trying to revert the changes

--- WeCareApp/app/src/main/java/ca/cmpt362/projects/WeCareApp/MainMenuActivity.kt
+++ WeCareApp/app/src/main/java/ca/cmpt362/projects/WeCareApp/MainMenuActivity.kt
@@ -12,7 +12,7 @@ import com.google.firebase.auth.FirebaseAuth
 class MainMenuActivity:AppCompatActivity() {
     private lateinit var signoutBtn:Button
     private lateinit var surveyBtn:Button
-    private lateinit var findClinicBtn:Button
+    // private lateinit var findClinicBtn:Button
     private lateinit var diaryBtn:Button
     private lateinit var meditationBtn:Button
 
@@ -22,16 +22,17 @@ class MainMenuActivity:AppCompatActivity() {
 
         signoutBtn = findViewById(R.id.signOut)
         surveyBtn = findViewById(R.id.SurveyBtnId)
-        findClinicBtn = findViewById(R.id.findClinicBtnId)
+        // findClinicBtn = findViewById(R.id.findClinicBtnId)
         diaryBtn = findViewById(R.id.diaryBtnId)
         meditationBtn = findViewById(R.id.meditationBtnId)
 
         surveyBtn.setOnClickListener {
             startActivity(Intent(this, SurveyActivity::class.java))
         }
+        /*
         findClinicBtn.setOnClickListener {
             startActivity(Intent(this, FindAClinicActivity::class.java))
-        }
+        }*/
         diaryBtn.setOnClickListener {
             startActivity(Intent(this, DiaryActivity::class.java))
         }
