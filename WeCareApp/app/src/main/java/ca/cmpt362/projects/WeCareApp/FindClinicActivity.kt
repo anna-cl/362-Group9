859bf41391f20831ce7645d803b0becb46079eab
Shraousy <sss56@sfu.ca>
2022-11-29 12:41:04 -0800

    Finishing the revert process

---
+++ WeCareApp/app/src/main/java/ca/cmpt362/projects/WeCareApp/FindClinicActivity.kt
@@ -0,0 +1,15 @@
+package ca.cmpt362.projects.weCareApp
+
+import android.content.Context
+import android.os.Bundle
+import android.util.AttributeSet
+import android.view.View
+import androidx.appcompat.app.AppCompatActivity
+import ca.cmpt362.projects.weCareApp.R
+
+class FindClinicActivity:AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_find_clinic)
+    }
+}
