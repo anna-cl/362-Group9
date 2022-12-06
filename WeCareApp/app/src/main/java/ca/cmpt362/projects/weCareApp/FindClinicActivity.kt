270af8fda19639f080a23d26c48593d7e09474c5
SuperLeaven <90525124+SuperLeaven@users.noreply.github.com>
2022-12-06 06:20:15 -0800

    Find a nearby

--- WeCareApp/app/src/main/java/ca/cmpt362/projects/weCareApp/FindClinicActivity.kt
+++ WeCareApp/app/src/main/java/ca/cmpt362/projects/weCareApp/FindClinicActivity.kt
@@ -1,15 +1,42 @@
 package ca.cmpt362.projects.weCareApp
 
-import android.content.Context
+import android.annotation.SuppressLint
+import android.net.Uri
 import android.os.Bundle
-import android.util.AttributeSet
-import android.view.View
+import android.webkit.WebView
+import android.webkit.WebViewClient
 import androidx.appcompat.app.AppCompatActivity
-import ca.cmpt362.projects.weCareApp.R
 
-class FindClinicActivity:AppCompatActivity() {
+class FindClinicActivity: AppCompatActivity() {
+    private lateinit var webView: WebView
+    private val URL = "https://www.google.ca/maps"
+
+    @SuppressLint("SetJavaScriptEnabled")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_find_clinic)
+        webView = findViewById(R.id.webpage)
+
+        webView.apply {
+            loadUrl(URL)
+            webViewClient = WebViewClient()
+            settings.javaScriptEnabled = true
+
+        }
+        val intent = intent
+        val data: Uri? = intent.data
+        if (data != null) {
+            val url = intent.dataString
+            webView.loadUrl(URL)
+        } else{
+            webView.loadUrl("https://www.google.ca/maps")
+        }
+    }
+    override fun onBackPressed() {
+        if (webView.canGoBack()) {
+            webView.goBack()
+        }else {
+            super.onBackPressed()
+        }
     }
 }
